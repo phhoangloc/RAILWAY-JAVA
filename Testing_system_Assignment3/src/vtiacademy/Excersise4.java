03b206722471351f16cee131915848bb3da0cc07
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2021-11-25 19:00:27 +0900

    Update Excersise4.java

--- Testing_system_Assignment3/src/vtiacademy/Excersise4.java
+++ Testing_system_Assignment3/src/vtiacademy/Excersise4.java
@@ -11,5 +11,21 @@ public class Excersise4 {
 		question3();
 		question4();
 	}
+//	Question 1:
+//		Nhập một xâu kí tự, đếm số lượng các từ trong xâu kí tự đó (các từ có 
+//		thể cách nhau bằng nhiều khoảng trắng );
+//		Question 2: 
+//		Nhập hai xâu kí tự s1, s2 nối xâu kí tự s2 vào sau xâu s1;
+//		Question 3: 
+//		Viết chương trình để người dùng nhập vào tên và kiểm tra, nếu tên chư 
+//		viết hoa chữ cái đầu thì viết hoa lên
+//		Question 4: 
+//		Viết chương trình để người dùng nhập vào tên in từng ký tự trong tên 
+//		của người dùng ra
+//		VD: 
+//		Người dùng nhập vào "Nam", hệ thống sẽ in ra
+//		"Ký tự thứ 1 là: N" 
+//		"Ký tự thứ 1 là: A" 
+//		"Ký tự thứ 1 là: M"
 
 }
